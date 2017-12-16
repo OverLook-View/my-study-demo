96f46b38597518cbf301493edf18065ce3d10c3b
OverlookView <492055737@qq.com>
2017-12-17 00:51:05 +0800

    @Transational

--- springboot-demo/src/main/java/com/sy/study/springboot/demo/service/DemoService.java
+++ springboot-demo/src/main/java/com/sy/study/springboot/demo/service/DemoService.java
@@ -4,10 +4,12 @@ import com.sy.study.springboot.demo.bean.Demo;
 import com.sy.study.springboot.demo.mapper.DemoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class DemoService {
     @Autowired
     private DemoMapper demoMapper;
