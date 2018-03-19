24347c6180a7c6161182b6585f8e561772752504
OverLook-View <492055737@qq.com>
2018-03-19 11:09:29 +0800

    docker-demo

---
+++ docker-demo/src/main/java/com/example/sy/docker/dockerdemo/DockerDemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.sy.docker.dockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "hello world";
+	}
+}
