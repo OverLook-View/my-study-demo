f0c90f40fd8e1f41114c26ed618f6153f9be0e3e
OverLook-View <492055737@qq.com>
2018-03-20 10:46:53 +0800

    security-demo

---
+++ security-demo/src/main/java/com/example/sy/security/securitydemo/controller/TestEndpointsController.java
@@ -0,0 +1,22 @@
+package com.example.sy.security.securitydemo.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestEndpointsController {
+
+    @GetMapping("/product/{id}")
+    public String getProduct(@PathVariable String id){
+        Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
+        return "product id is "+id;
+    }
+    @GetMapping("/order/{id}")
+    public String getOrder(@PathVariable String id){
+        Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
+        return "order id is "+id;
+    }
+}
