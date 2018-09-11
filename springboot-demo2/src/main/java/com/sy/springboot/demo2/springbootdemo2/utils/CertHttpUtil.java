3c80e6402302833b9a7ba42dc4165cf3a1445036
OverLook-View <492055737@qq.com>
2018-09-11 16:59:43 +0800

    spring boot demo2

---
+++ springboot-demo2/src/main/java/com/sy/springboot/demo2/springbootdemo2/utils/CertHttpUtil.java
@@ -0,0 +1,79 @@
+package com.sy.springboot.demo2.springbootdemo2.utils;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.util.EntityUtils;
+
+import javax.net.ssl.SSLContext;
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.KeyStore;
+
+/**
+ * @description: http工具类（带证书）
+ * @author: sheny
+ * @create: 2018-09-11 10:51
+ **/
+public class CertHttpUtil {
+    private static int socketTimeout = 10000;// 连接超时时间，默认10秒
+    private static int connectTimeout = 30000;// 传输超时时间，默认30秒
+
+    /**
+     * 通过Https往API post xml数据
+     *
+     * @param url      API地址
+     * @param xmlObj   要提交的XML数据对象
+     * @param mchId    商户ID
+     * @param certPath 证书位置
+     * @return
+     */
+    public static String postData(String url, String xmlObj, String mchId, String certPath) throws Exception {
+        CloseableHttpClient httpClient;// HTTP请求器
+        // 加载证书
+        // 证书密码，默认为商户ID
+        String key = mchId;
+        // 证书的路径
+        String path = certPath;
+        // 指定读取证书格式为PKCS12
+        KeyStore keyStore = KeyStore.getInstance("PKCS12");
+        // 读取本机存放的PKCS12证书文件
+        FileInputStream instream = new FileInputStream(new File(path));
+        try {
+            // 指定PKCS12的密码(商户ID)
+            keyStore.load(instream, key.toCharArray());
+        } finally {
+            instream.close();
+        }
+        SSLContext sslcontext = SSLContexts.custom().loadKeyMaterial(keyStore, key.toCharArray()).build();
+        SSLConnectionSocketFactory sslsf =
+                new SSLConnectionSocketFactory(sslcontext, new String[]{"TLSv1"}, null,
+                        SSLConnectionSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER);
+        httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
+        String result = null;
+        HttpPost httpPost = new HttpPost(url);
+        // 得指明使用UTF-8编码，否则到API服务器XML的中文不能被成功识别
+        StringEntity postEntity = new StringEntity(xmlObj, "UTF-8");
+        httpPost.addHeader("Content-Type", "text/xml");
+        httpPost.setEntity(postEntity);
+        // 根据默认超时限制初始化requestConfig请求器的配置
+        RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(socketTimeout).setConnectTimeout(connectTimeout).build();
+        // 设置请求器的配置
+        httpPost.setConfig(requestConfig);
+        try {
+            HttpResponse response = httpClient.execute(httpPost);
+            HttpEntity entity = response.getEntity();
+            result = EntityUtils.toString(entity, "UTF-8");
+        } finally {
+            httpPost.abort();
+        }
+        return result;
+    }
+
+}
