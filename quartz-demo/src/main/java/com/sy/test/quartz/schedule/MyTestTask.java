cc4590c82b850d2d99970d23260c850fe29385c6
OverLook-View <492055737@qq.com>
2018-10-18 15:31:16 +0800

    static schedule task

---
+++ quartz-demo/src/main/java/com/sy/test/quartz/schedule/MyTestTask.java
@@ -0,0 +1,18 @@
+package com.sy.test.quartz.schedule;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @description:
+ * @author: sheny
+ * @create: 2018-10-18 15:12
+ **/
+@Component
+public class MyTestTask {
+
+    public void testTask(){
+        System.out.println("测试任务执行了-------------- "+new Date());
+    }
+}
