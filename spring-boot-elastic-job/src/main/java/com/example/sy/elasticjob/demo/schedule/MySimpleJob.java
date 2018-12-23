7ae8c93070ad73b9a6d274f500e0a105cff194c8
OverlookView <492055737@qq.com>
2018-12-23 21:42:16 +0800

    init

---
+++ spring-boot-elastic-job/src/main/java/com/example/sy/elasticjob/demo/schedule/MySimpleJob.java
@@ -0,0 +1,28 @@
+package com.example.sy.elasticjob.demo.schedule;
+
+import com.dangdang.ddframe.job.api.ShardingContext;
+import com.dangdang.ddframe.job.api.simple.SimpleJob;
+import com.dangdang.elasticjob.lite.annotation.ElasticSimpleJob;
+
+/**
+ * @description: 测试定时任务
+ * @author: OverlookView
+ * @create: 2018-12-23 18:39
+ **/
+@ElasticSimpleJob(cron = "0 * * * * ?", jobName = "testJob", shardingTotalCount = 2, jobParameter = "测试参数", shardingItemParameters = "0=A,1=B")
+public class MySimpleJob implements SimpleJob {
+    @Override
+    public void execute(ShardingContext shardingContext) {
+        System.out.println(String.format("------Thread ID: %s, 任务总片数: %s, "
+                        + "当前分片项: %s.当前参数: %s,"
+                        + "当前任务名称: %s.当前任务参数: %s"
+                ,
+                Thread.currentThread().getId(),
+                shardingContext.getShardingTotalCount(),
+                shardingContext.getShardingItem(),
+                shardingContext.getShardingParameter(),
+                shardingContext.getJobName(),
+                shardingContext.getJobParameter()
+        ));
+    }
+}
