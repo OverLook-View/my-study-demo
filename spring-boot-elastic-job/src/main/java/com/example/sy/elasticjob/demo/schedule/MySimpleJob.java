8ae19fc7f9924dff103cb13d35777a0920399e17
OverLook-View <492055737@qq.com>
2018-12-25 08:58:13 +0800

    update

--- spring-boot-elastic-job/src/main/java/com/example/sy/elasticjob/demo/schedule/MySimpleJob.java
+++ spring-boot-elastic-job/src/main/java/com/example/sy/elasticjob/demo/schedule/MySimpleJob.java
@@ -3,6 +3,9 @@ package com.example.sy.elasticjob.demo.schedule;
 import com.dangdang.ddframe.job.api.ShardingContext;
 import com.dangdang.ddframe.job.api.simple.SimpleJob;
 import com.dangdang.elasticjob.lite.annotation.ElasticSimpleJob;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 /**
  * @description: 测试定时任务
@@ -10,12 +13,14 @@ import com.dangdang.elasticjob.lite.annotation.ElasticSimpleJob;
  * @create: 2018-12-23 18:39
  **/
 @ElasticSimpleJob(cron = "0 * * * * ?", jobName = "testJob", shardingTotalCount = 2, jobParameter = "测试参数", shardingItemParameters = "0=A,1=B")
+@Component
 public class MySimpleJob implements SimpleJob {
     @Override
     public void execute(ShardingContext shardingContext) {
         System.out.println(String.format("------Thread ID: %s, 任务总片数: %s, "
                         + "当前分片项: %s.当前参数: %s,"
                         + "当前任务名称: %s.当前任务参数: %s"
+                        + " 时间:" + new Date()
                 ,
                 Thread.currentThread().getId(),
                 shardingContext.getShardingTotalCount(),
