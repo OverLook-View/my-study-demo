b740956bd6b8b60bd1e993f6fd0194bf7cf39fc1
OverLook-View <492055737@qq.com>
2021-03-12 14:48:59 +0800

    tree

---
+++ data-structure-study/src/com/study/demo/tree/TreeNode.java
@@ -0,0 +1,13 @@
+package com.study.demo.tree;
+
+/**
+ * @description:
+ * @author: OverlookView
+ * @create: 2021-03-12 10:02
+ **/
+public class TreeNode<AnyType> {
+    AnyType element;
+    TreeNode firstChild;
+    TreeNode nextSibling;
+
+}
