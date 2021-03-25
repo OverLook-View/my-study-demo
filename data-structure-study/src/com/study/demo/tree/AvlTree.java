a9b554f68ed07f9d79e462d25aeae3dfb88b8756
OverLook-View <492055737@qq.com>
2021-03-25 18:59:45 +0800

    avl tree

--- data-structure-study/src/com/study/demo/tree/AvlTree.java
+++ data-structure-study/src/com/study/demo/tree/AvlTree.java
@@ -22,5 +22,106 @@ public class AvlTree<AnyType extends Comparable<? super AnyType>> extends Binary
             this.right = rt;
             this.height = 0;
         }
+
+    }
+
+    private int height(AvlNode<AnyType> t) {
+        return t == null ? -1 : t.height;
+    }
+
+    private AvlNode<AnyType> insert(AnyType x, AvlNode<AnyType> t) {
+        if (t == null)
+            return new AvlNode<>(x, null, null);
+        int compareResult = x.compareTo(t.element);
+
+        if (compareResult < 0)
+            t.left = insert(x, t.left);
+        else if (compareResult > 0)
+            t.right = insert(x, t.right);
+        else
+            ;//Duplicate; do nothing
+        return balance(t);
+    }
+
+    private static final int ALLOWED_IMBALANCE = 1;
+
+    private AvlNode<AnyType> balance(AvlNode<AnyType> t) {
+        if (t == null)
+            return t;
+        if (height(t.left) - height(t.right) > ALLOWED_IMBALANCE) {
+            if (height(t.left.left) >= height(t.left.right))
+                t = rotateWithLeftChild(t);
+            else
+                t = doubleWithLeftChild(t);
+        } else if (height(t.right) - height(t.left) > ALLOWED_IMBALANCE) {
+            if (height(t.right.right) >= height(t.right.left))
+                t = rotateWithRightChild(t);
+            else
+                t = doubleWithRightChild(t);
+        }
+
+        t.height = Math.max(height(t.left), height(t.right)) + 1;
+        return t;
+    }
+
+    private AvlNode<AnyType> rotateWithRightChild(AvlNode<AnyType> k2) {
+        AvlNode<AnyType> k1=k2.right;
+        k2.right=k1.left;
+        k1.left=k2;
+        k2.height=Math.max(height(k2.left),height(k2.right))+1;
+        k1.height=Math.max(height(k1.right),k2.height)+1;
+        return k1;
+    }
+
+    private AvlNode<AnyType> rotateWithLeftChild(AvlNode<AnyType> k2) {
+        AvlNode<AnyType> k1=k2.left;
+        k2.left=k1.right;
+        k1.right=k2;
+        k2.height=Math.max(height(k2.left),height(k2.right))+1;
+        k1.height=Math.max(height(k1.left),k2.height)+1;
+        return k1;
+    }
+
+    private AvlNode<AnyType> doubleWithRightChild(AvlNode<AnyType> k3){
+        k3.right=rotateWithLeftChild(k3.right);
+        return rotateWithRightChild(k3);
+    }
+
+    private AvlNode<AnyType> doubleWithLeftChild(AvlNode<AnyType> k3){
+        k3.left=rotateWithRightChild(k3.left);
+        return rotateWithLeftChild(k3);
+    }
+
+    private AvlNode<AnyType> remove(AnyType x, AvlNode<AnyType> t){
+        if (t==null)
+            return t;
+        int compareResult = x.compareTo(t.element);
+
+        if (compareResult<0)
+            t.left=remove(x,t.left);
+        else if (compareResult>0){
+            t.right=remove(x,t.right);
+        } else if (t.left!=null && t.right!=null){
+            t.element=findMin(t.right).element;
+            t.right=remove(t.element,t.right);
+        } else {
+            t=(t.left!=null)?t.left:t.right;
+        }
+        return balance(t);
+    }
+
+    private AvlNode<AnyType> findMin(AvlNode<AnyType> t) {
+        if (t == null)
+            return null;
+        else if (t.left == null)
+            return t;
+        return findMin(t.left);
+    }
+
+    private AvlNode<AnyType> findMax(AvlNode<AnyType> t) {
+        if (t != null)
+            while (t.right != null)
+                t = t.right;
+        return t;
     }
 }
