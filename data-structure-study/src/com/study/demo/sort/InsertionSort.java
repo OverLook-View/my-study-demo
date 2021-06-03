427f334daf2c236630f9529aa4a379b270135f1d
OverLook-View <492055737@qq.com>
2021-06-03 16:51:36 +0800

    sort

---
+++ data-structure-study/src/com/study/demo/sort/InsertionSort.java
@@ -0,0 +1,14 @@
+package com.study.demo.sort;
+
+public class InsertionSort {
+    public static <AnyType extends Comparable<? super AnyType>> void insertionSort(AnyType[] a) {
+        int j;
+        for (int p = 0; p < a.length; p++) {
+            AnyType tmp = a[p];
+            for (j = p; j > 0 && tmp.compareTo(a[j - 1]) < 0; j--) {
+                a[j] = a[j - 1];
+            }
+            a[j] = tmp;
+        }
+    }
+}
