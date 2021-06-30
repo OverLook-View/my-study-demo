a64f320f18812d4b412eba88b3004e98b73abdfc
OverLook-View <492055737@qq.com>
2021-06-30 15:52:19 +0800

    QuickSort

--- data-structure-study/src/com/study/demo/sort/QuickSort.java
+++ data-structure-study/src/com/study/demo/sort/QuickSort.java
@@ -29,4 +29,55 @@ public class QuickSort {
             list.addAll(larger);
         }
     }
+
+    public static <AnyType extends Comparable<? super AnyType>> void quicksort(AnyType[] a) {
+        quicksort(a, 0, a.length - 1);
+    }
+
+    private static final int CUTOFF = 10;
+
+    private static <AnyType extends Comparable<? super AnyType>> void quicksort(AnyType[] a, int left, int right) {
+        if (left + CUTOFF <= right) {
+            AnyType pivot = median3(a, left, right);
+
+            int i = left, j = right - 1;
+            for (; ; ) {
+                while ((a[++i].compareTo(pivot) < 0)) {
+                }
+                while ((a[--j].compareTo(pivot) > 0)) {
+                }
+                if (i < j) {
+                    swapReferences(a, i, j);
+                } else {
+                    break;
+                }
+
+                swapReferences(a, i, right - 1);
+
+                quicksort(a, left, i - 1);
+                quicksort(a, i + 1, right);
+            }
+        } else {
+            InsertionSort.insertionSort(a);
+        }
+    }
+
+    private static <AnyType extends Comparable<? super AnyType>> void swapReferences(AnyType[] a, int i, int i1) {
+        AnyType temp = a[i];
+        a[i] = a[i1];
+        a[i1] = temp;
+    }
+
+    private static <AnyType extends Comparable<? super AnyType>> AnyType median3(AnyType[] a, int left, int right) {
+        int center = (left + right) / 2;
+        if (a[center].compareTo(a[left]) < 0)
+            swapReferences(a, left, center);
+        if (a[right].compareTo(a[left]) < 0)
+            swapReferences(a, left, right);
+        if (a[right].compareTo(a[center]) < 0)
+            swapReferences(a, center, right);
+
+        swapReferences(a, center, right - 1);
+        return a[right - 1];
+    }
 }
