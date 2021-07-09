ebbca48b69ca7f677d9c1e6352713eda94968d46
OverLook-View <492055737@qq.com>
2021-07-09 14:10:02 +0800

    RadixSort

--- data-structure-study/src/com/study/demo/sort/RadixSort.java
+++ data-structure-study/src/com/study/demo/sort/RadixSort.java
@@ -56,4 +56,45 @@ public class RadixSort {
             }
         }
     }
+
+    public static void radixSort(String[] arr, int maxLen) {
+        final int BUCKETS = 256;
+
+        ArrayList<String>[] wordsByLength = new ArrayList[maxLen + 1];
+        ArrayList<String>[] buckets = new ArrayList[BUCKETS];
+
+        for (int i = 0; i < wordsByLength.length; i++) {
+            wordsByLength[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = new ArrayList<>();
+        }
+
+        for (String s : arr) {
+            wordsByLength[s.length()].add(s);
+        }
+
+        int idx = 0;
+        for (ArrayList<String> wordList : wordsByLength) {
+            for (String s : wordList) {
+                arr[idx++] = s;
+            }
+        }
+
+        int startingIndex = arr.length;
+        for (int pos = maxLen - 1; pos >= 0; pos--) {
+            startingIndex -= wordsByLength[pos + 1].size();
+
+            for (int i = startingIndex; i < arr.length; i++) {
+                buckets[arr[i].charAt(pos)].add(arr[i]);
+            }
+            idx = startingIndex;
+            for (ArrayList<String> thisBucket : buckets) {
+                for (String s : thisBucket) {
+                    arr[idx++] = s;
+                }
+                thisBucket.clear();
+            }
+        }
+    }
 }
